3fe67efa223f413af36de1c1d57a23e0fc56e48f
Kieran Mc Gowan <kieranmcgowan21@homtail.com>
2023-01-28 13:43:13 +0000

    init commit

---
+++ src/components/AddTask.js
@@ -0,0 +1,56 @@
+import { useState } from "react";
+
+const AddTask = ({ addTask }) => {
+  const [text, setText] = useState("");
+  const [day, setDay] = useState("");
+  const [reminder, setReminder] = useState(false);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    if (!text) {
+      alert("Please add a task");
+      return;
+    }
+
+    addTask({ text, day, reminder });
+
+    setText("");
+    setDay("");
+    setReminder(false);
+    console.log("done");
+  };
+
+  return (
+    <form className="add-form" onSubmit={onSubmit}>
+      <div className="form-control">
+        <label>Task</label>
+        <input
+          type="text"
+          placeholder="Task"
+          onChange={(e) => setText(e.target.value)}
+        ></input>
+      </div>
+      <div className="form-control">
+        <label>Day & Time</label>
+        <input
+          type="text"
+          placeholder="Day and Time"
+          onChange={(e) => setDay(e.target.value)}
+        ></input>
+      </div>
+      <div className="form-control form-control-check">
+        <label>Set Reminder</label>
+        <input
+          type="checkbox"
+          checked={reminder}
+          value={reminder}
+          onChange={(e) => setReminder(e.currentTarget.checked)}
+        ></input>
+      </div>
+      <input type="submit" className="btn btn-block"></input>
+    </form>
+  );
+};
+
+export default AddTask;
